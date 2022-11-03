5567e507b8fbae5f9a43df25279f7c0da35565e9
rohity20 <2020.rohit.yadav@ves.ac.in>
2022-11-04 02:04:22 +0530

    commited

---
+++ backend/config/database.js
@@ -0,0 +1,32 @@
+
+const mongoose = require("mongoose");
+
+
+
+const connectDatabase = () => {
+    // mongoose.connect("process.env.DB_URI",{useNewUrlParser:true,useUnifiedTopoplogy:true,useCreateIndex:true}).then(
+    mongoose.connect('mongodb://localhost:27017/Ecommerce', { useNewUrlParser: true }).then(
+        (data) => {
+            console.log(`Mongodb connected with server: ${data.connection.host}`);
+
+        });
+};
+
+module.exports = connectDatabase
+
+
+// const mongoose = require("mongoose");
+
+// const connectDatabase = () => {
+//   mongoose
+//     .connect(process.env.DB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//     })
+//     .then((data) => {
+//       console.log(`Mongodb connected with server: ${data.connection.host}`);
+//     });
+// };
+
+// module.exports = connectDatabase;
